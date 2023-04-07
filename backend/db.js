644515ee2c9f6d229a1ea18c5569f9ab9a6609b6
mohitcontata <mohitb@contata.com>
2023-04-07 12:52:28 +0530

    Installed react rout dom package

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotbook";
+const mongoURI = "mongodb://127.0.0.1:27017/inotbook";
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI);
+const connectToMongo = async ()=>{
+    await mongoose.connect(mongoURI);
     console.log("Connected to Mongo Successfully!!");
 }
 
